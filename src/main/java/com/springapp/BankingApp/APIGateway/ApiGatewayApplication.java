18c9365191c11d5af567c349af127c1de3597626
Tuhin Pal <tuhinpal.434@gmail.com>
2021-07-21 18:52:37 +0530

    Adding SpringBootServletInitializer

--- src/main/java/com/springapp/BankingApp/APIGateway/ApiGatewayApplication.java
+++ src/main/java/com/springapp/BankingApp/APIGateway/ApiGatewayApplication.java
@@ -2,11 +2,18 @@ package com.springapp.BankingApp.APIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ApiGatewayApplication {
+public class ApiGatewayApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ApiGatewayApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
